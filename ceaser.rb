d381a9964dda2cab8a4f8f4ffabdfae6854b32c7
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-08 17:45:30 -0700

    hail ceaser encryption working

--- ceaser.rb
+++ ceaser.rb
@@ -1,16 +1,23 @@
 require 'pry'
 
 class Ceaser
-
+  attr_reader :message, :rotation, :character_map
   def initialize(rotation, message)
     @rotation = rotation
-    @message = message.join.chars
-    binding.pry
+    @message = message.chars
     @character_map = ('a'..'z').to_a
+  end
+
+  def wrap
+    message.map { |letter| index = character_map.index(letter)
+                       new_index = (index + rotation) % 26
+                       character_map[new_index]
+                }.join
   end
 
 end
 
 if __FILE__ == $PROGRAM_NAME
-  Ceaser.new(ARGV[0], ARGV[1..-1])
+  ceaser = Ceaser.new(5, "hello")
+  puts ceaser.wrap
 end
